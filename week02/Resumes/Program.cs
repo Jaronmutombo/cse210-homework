6323b0c3e6ba006c34aa56e3c3281fff97ce9d1e
Jaronmutombo <kalungamutombo@gmail.com>
2026-03-11 17:58:55 -0600

    Week 2 exercise

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Data Analyst";
+        job1._company = "Tesla";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Customer Relations Manager";
+        job2._company = "Natures Sunshine";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Dalma Ebondo";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }

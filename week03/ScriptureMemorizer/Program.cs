87f5c22b9f6886f31792a5796f2e2a45a96d1999
Jaronmutombo <kalungamutombo@gmail.com>
2026-03-21 15:42:01 -0600

    Memo

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Reference reference = new Reference("Matthew", 6, 9 - 13);
+        Reference reference = new Reference("Matthew Chapter:", 6, 9 - 13);
 
         string text = "Our Father which art in heaven, Hallowed be thy name. Thy kingdom come. Thy will be done in earth, as it is in heaven. Give us this day our daily bread. And forgive us our debts, as we forgive our debtors. And lead us not into temptation, but deliver us from evil: For thine is the kingdom, and the power, and the glory, for ever. Amen.";
 
